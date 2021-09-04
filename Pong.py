45c9cb21823bdf4e1b9d710014ad31e55ac9a396
Void <vincent.heddergott@gmail.com>
2021-09-04 12:03:39 +0200

    Added Movement + Border Collision Detection

--- Pong.py
+++ Pong.py
@@ -7,7 +7,7 @@ wn.setup(width=800, height=600)
 wn.tracer(0)
 
 
-#* Paddle 1
+# Paddle 1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
 paddle_1.shape("square")
@@ -17,7 +17,7 @@ paddle_1.penup()
 paddle_1.goto(-350, 0)
 
 
-#*Paddle 2
+# Paddle 2
 paddle_2 = turtle.Turtle()
 paddle_2.speed(0)
 paddle_2.shape("square")
@@ -27,16 +27,19 @@ paddle_2.penup()
 paddle_2.goto(350, 0)
 
 
-#* Ball
+# Ball
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = 0.1
 
 
-#* Function Paddle 1
+
+# Function Paddle 1
 def paddle_1_up():
     y = paddle_1.ycor()
     y += 20
@@ -48,7 +51,7 @@ def paddle_1_down():
     paddle_1.sety(y)
 
 
-#* Function Paddle 2
+# Function Paddle 2
 def paddle_2_up():
     y = paddle_2.ycor()
     y += 20
@@ -59,7 +62,7 @@ def paddle_2_down():
     y -= 20
     paddle_2.sety(y)
 
-#* Keyboard Bindings
+# Keyboard Bindings
 wn.listen()
 wn.onkeypress(paddle_1_up, "w")
 wn.onkeypress(paddle_1_down, "s")
@@ -70,14 +73,27 @@ wn.onkeypress(paddle_2_down, "Down")
 
 
 
-
-
-
-
-
-
-
-#* Main Game Loop
+# Main Game Loop
 while True:
     wn.update()
 
+    # Ball Movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
